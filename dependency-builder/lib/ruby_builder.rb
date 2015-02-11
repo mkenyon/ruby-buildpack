a7186368ff4b24326a6604bccc9a8dcc2f5eb563
Anthony Emengo and Rasheed Abdul-Aziz <pair+aemengo+squeedee@pivotal.io>
2015-02-11 15:57:06 -0500

    Added ruby build smoke tests
    [#74235794]

--- dependency-builder/lib/ruby_builder.rb
+++ dependency-builder/lib/ruby_builder.rb
@@ -17,27 +17,57 @@ class MiniPortile
   end
 end
 
-recipe = MiniPortile.new("ruby", ARGV[0].to_s)
+version, patch = ARGV
 
-recipe.files = [ARGV[1]]
+recipe = MiniPortile.new("ruby", version)
+
+source_uri ="http://cache.ruby-lang.org/pub/ruby/1.9/ruby-#{version}-p#{patch}.tar.gz"
+recipe.files = [source_uri]
+
+flags_to_remove = %w{
+  --enable-static
+  --disable-shared
+}
+
+recipe.configure_options.reject! { |opt| flags_to_remove.include? opt }
+
 recipe.configure_options << "--enable-load-relative"
 recipe.configure_options << "--disable-install-doc"
 recipe.configure_options << "--disable-static"
 recipe.configure_options << "--enable-shared"
 
-checkpoint = ".#{recipe.name}-#{recipe.version}.installed"
+puts "-----> Using config options: "
+recipe.configure_options.each { |opt| puts "\t#{opt}" }
+puts
 
-if File.exist?(checkpoint)
-  puts "INFO: Already compiled"
-else
-  puts "-----> Using config options: "
-  recipe.configure_options.each { |opt| puts "\t#{opt}" }
-  puts
+recipe.cook
 
-  recipe.cook
+# TEST
+puts "------> Running Tests"
 
-  `touch ${checkpoint}`
+actual_version = `cd #{recipe.path}; bin/ruby -v`
+expected_version = "#{version}p#{patch}"
+
+unless actual_version.include? expected_version
+  puts "Ruby Version did not match"
+  puts "Expected: #{actual_version}"
+  puts "To Include: #{expected_version}"
+  exit 1
 end
+print "."
+
+actual_output = `cd #{recipe.path}; bin/ruby -e 'print "hello"'`
+expected_output = 'hello'
+if actual_output != expected_output
+  puts "Ruby did not execute as expected"
+  puts "Expected: #{actual_output}"
+  puts "To Equal: #{expected_output}"
+  exit 1
+end
+
+print "."
+
+puts "\nTests passed!"
 
 puts "Temp build in #{recipe.path}"
 
